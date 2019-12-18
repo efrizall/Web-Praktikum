7ac0f5c56dea2cc3b58fd0d559129c0e843be28c
efrizall <rizal.ef340@gmail.com>
2019-12-18 10:13:54 +0700

    initial commit

---
+++ Praktikum07/variable_user.php
@@ -0,0 +1,14 @@
+<?php 
+
+	// Mendefinisikan variable
+	$nama = 'Rosalie Naurah';
+	$umur = 18;
+	$berat = 12.5;
+
+	echo 'Nama : ' . $nama;
+	echo '<br>Umur : ' . $umur . 'Tahun';
+	echo '<br>Berat : ' . $berat . 'Kg';
+
+	echo '<br> Hello ' . $nama . ", apa kabar?";
+
+ ?>
